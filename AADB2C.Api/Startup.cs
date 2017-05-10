bcea010ed20523e80bd57969984da4d429c7e483
suryakc <kiran.cs@gmail.com>
2017-05-10 16:46:20 +0530

    First commit

---
+++ AADB2C.Api/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AADB2C.Api
+{
+    public class Startup
+    {
+        // These values are pulled from web.config
+        public static string AadInstance = ConfigurationManager.AppSettings["ida:AadInstance"];
+        public static string Tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        public static string ClientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        public static string SignUpPolicy = ConfigurationManager.AppSettings["ida:SignUpPolicyId"];
+        public static string SignInPolicy = ConfigurationManager.AppSettings["ida:SignInPolicyId"];
+        public static string EditProfilePolicy = ConfigurationManager.AppSettings["ida:UserProfilePolicyId"];
+
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            // web api routes...
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            app.UseOAuthBearerAuthentication(CreateBearerOptionsFromPolicy(SignUpPolicy));
+            app.UseOAuthBearerAuthentication(CreateBearerOptionsFromPolicy(SignInPolicy));
+            app.UseOAuthBearerAuthentication(CreateBearerOptionsFromPolicy(EditProfilePolicy));
+        }
+
+        private OAuthBearerAuthenticationOptions CreateBearerOptionsFromPolicy(string policy)
+        {
+            var metadataEndpoint = string.Format(AadInstance, Tenant, policy);
+
+            TokenValidationParameters tvps = new TokenValidationParameters
+            {
+                // This is where you specify that your API only accepts tokens from its own clients
+                ValidAudience = ClientId,
+                AuthenticationType = policy,
+                NameClaimType = "http://schemas.microsoft.com/identity/claims/objectidentifier"
+            };
+
+            return new OAuthBearerAuthenticationOptions
+            {
+                // This SecurityTokenProvider fetches the Azure AD B2C metadata & signing keys from the OpenIDConnect metadata endpoint
+                AccessTokenFormat = new JwtFormat(tvps, new OpenIdConnectCachingSecurityTokenProvider(metadataEndpoint))
+            };
+        }
+    }
+}
---
+++ AADB2C.WebClientMvc/Controllers/OrdersController.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AADB2C.WebClientMvc.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private static string serviceUrl = ConfigurationManager.AppSettings["api:OrdersApiUrl"];
+
+        // GET: Orders
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+
+                var bootstrapContext = ClaimsPrincipal.Current.Identities.First().BootstrapContext as System.IdentityModel.Tokens.BootstrapContext;
+
+                HttpClient client = new HttpClient();
+
+                client.BaseAddress = new Uri(serviceUrl);
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bootstrapContext.Token);
+
+                HttpResponseMessage response = await client.GetAsync("api/orders");
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var orders = await response.Content.ReadAsAsync<List<OrderModel>>();
+
+                    return View(orders);
+                }
+                else
+                {
+                    // If the call failed with access denied, show the user an error indicating they might need to sign-in again.
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        return new RedirectResult("/Error?message=Error: " + response.ReasonPhrase + " You might need to sign in again.");
+                    }
+                }
+
+                return new RedirectResult("/Error?message=An Error Occurred Reading Orders List: " + response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                return new RedirectResult("/Error?message=An Error Occurred Reading Orders List: " + ex.Message);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind(Include = "ShipperName,ShipperCity")]OrderModel order)
+        {
+
+            try
+            {
+                var bootstrapContext = ClaimsPrincipal.Current.Identities.First().BootstrapContext as System.IdentityModel.Tokens.BootstrapContext;
+
+                HttpClient client = new HttpClient();
+
+                client.BaseAddress = new Uri(serviceUrl);
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bootstrapContext.Token);
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/orders", order);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // If the call failed with access denied, show the user an error indicating they might need to sign-in again.
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        return new RedirectResult("/Error?message=Error: " + response.ReasonPhrase + " You might need to sign in again.");
+                    }
+                }
+
+                return new RedirectResult("/Error?message=An Error Occurred Creating Order: " + response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                return new RedirectResult("/Error?message=An Error Occurred Creating Order: " + ex.Message);
+            }
+
+        }
+
+    }
+
+    public class OrderModel
+    {
+        public string OrderID { get; set; }
+        [Display(Name = "Shipper")]
+        public string ShipperName { get; set; }
+        [Display(Name = "Shipper City")]
+        public string ShipperCity { get; set; }
+        public DateTimeOffset TS { get; set; }
+    }
+}
